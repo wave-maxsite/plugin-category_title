001c970cb835799fd74e3b5e965af7c1c443e663
Wave <wave.blessed@gmail.com>
2015-03-27 20:46:59 +0200

    ver. 1.1

---
+++ category_title/info.php
@@ -0,0 +1,12 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+
+$info = array(
+	'name' => 'Category title',
+	'description' => 'Позволяет задавать рубрикам keywords, title и description',
+	'version' => '1.1',
+	'author' => 'Wave',
+	'plugin_url' => 'http://wave.fantregata.com/page/category-title',
+	'author_url' => 'http://wave.fantregata.com/',
+	'group' => 'template'
+);
+
